1a0c3dfb15cbfb7791ac1f318cfc10e6b68b48d9
Lemon <lisandro.m.yo@gmail.com>
2022-12-21 23:21:11 -0300

    update

---
+++ Loas_data.sql
@@ -0,0 +1,86 @@
+INSERT INTO clients
+VALUES(
+    1,
+    "Pepe Ramirez",
+    63725619,
+    "peperamirez@gmail.com",
+    "Paseo Iria, 499, Bajos",
+    989907450,
+    "honor shard pouch(L)"
+);
+
+INSERT INTO clients
+VALUES(
+    2,
+    "Biel Arce",
+    39252267,
+    "aya.saldivar@latinmail.com",
+    "Avenida Armenta, 113, Bajo 2º",
+    34676991751,
+    "honor shard pouch(M)"
+);
+
+INSERT INTO clients
+VALUES(
+    3,
+    "Emma Sanabria Tercero",
+    35111183,
+    "miramontes.saul@giron.es",
+    "Travesía Cuenca, 014, 65º A",
+    34658667082,
+    "honor shard pouch(S)"
+);
+
+INSERT into products
+VALUES(
+    1,
+    19548,
+    "honor shard pouch(S)",
+    50,
+    "Shards"
+);
+
+INSERT into products
+VALUES(
+    2,
+    14039,
+    "honor shard pouch(M)",
+    59,
+    "Shards"
+);
+
+INSERT into products
+VALUES(
+    3,
+    21542,
+    "honor shard pouch(L)",
+    55,
+    "Shards"
+);
+
+INSERT into providers
+VALUES(
+    1,
+    "Antonio Reyna",
+    "luciana.leiva@hotmail.com",
+    "Cl.Ximena Arroyo #04285",
+    51 149 7512
+)
+
+INSERT into providers
+VALUES(
+    2,
+    "Andrea Carrasco Rivera",
+    "malena68@mateo.com",
+    "Av. Adrián Muñiz # 6050",
+    1350065
+)
+
+INSERT into providers
+VALUES(
+    3,
+    "Jerónimo Valadez",
+    "montenegro.alexander@gmail.com",
+    "Jr.Ivan Meléndez #60269",
+    511023404
+)
