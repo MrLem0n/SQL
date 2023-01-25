7ee2a6fc1e0284b58aedf6a8001c47ee3db7250a
Lemon <lisandro.m.yo@gmail.com>
2023-01-25 11:59:05 -0300

    ProceduresX2

--- Stored Procedures.sql
+++ Stored Procedures.sql
@@ -21,3 +21,57 @@ END CASE;
 
 END
 //
+
+--------------------------------------------------------------------------------------------------------------
+
+DELIMITER //
+Create Procedure `alterTables`
+(
+id_prod INT,
+code_prod INT,
+name_prod VARCHAR(60),
+stock_prod INT,
+cat_prod VARCHAR(30),
+operacion Varchar(20)
+)
+
+BEGIN
+IF operacion = "Insert" THEN
+	BEGIN
+		INSERT INTO products
+					(prod_id,
+                    prod_code,
+                    prod_name,
+                    prod_stock,
+                    prod_cat)
+                    
+		VALUES		(id_prod,
+					code_prod,
+                    name_prod,
+                    stock_prod,
+                    cat_prod);
+	END;
+    
+ELSEIF operacion = 'Select' THEN
+        BEGIN
+            SELECT *
+            FROM   products;
+        END;
+
+ELSEIF operacion = 'Update' THEN
+        BEGIN
+            UPDATE products
+            SET    prod_code = code_prod,
+                   prod_name = name_prod,
+                   prod_stock = stock_prod,
+                   prod_cat = cat_prod
+            WHERE  prod_id = id_prod;
+        END;
+ELSEIF operacion = 'Delete' THEN
+        BEGIN
+            DELETE FROM products
+            WHERE  prod_id = id_prod;
+        END;
+END IF;
+END 
+//
